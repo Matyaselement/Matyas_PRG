716b1139754e9b358b69c0bfdee45965bc1162e5
Matyaselement <matyas.voboril@gmail.com>
2023-09-20 11:40:27 +0200

    aktualizace kalkulačky

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Toto je kalkulačka");
+            Console.WriteLine("Vítejte v šetřiči mysli (kalkulačce)");
+
+            Console.WriteLine("Pokud chcete sčítat, odčítat, násobit nebo dělit, stistkněte 1, pokud umocňovat, stiskněte 2: ");
+            int o1 = Convert.ToInt32(Console.ReadLine());
+            int o2 = Convert.ToInt32(Console.ReadLine());
+
+            if (o1 == "1")
 
             Console.Write("První číslo: ");
             int c1 = Convert.ToInt32(Console.ReadLine());
@@ -23,10 +30,12 @@ namespace Calculator
             Console.Write("Druhé číslo: ");
             int c2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Operace(+, -, *, /): ");
+            Console.Write("Operace(+, -, *, /, 224): ");
             string op = Console.ReadLine();
 
             int result = 0;
+
+            bool success = true;
 
             if (op == "+")
             {
@@ -42,52 +51,38 @@ namespace Calculator
             }
             else if (op == "/")
             {
-                if (c1 != 0)
+                if (c2 == 0)
                 {
-                    result = c1 / c2;
+                    Console.WriteLine("Chyba, 0 nekamarádí s dělením");
+                    success = false;
                 }
                 else
                 {
-                    Console.WriteLine("Chyba, 0 nekamarádí s dělením");
+                    result = c1 / c2;
                 }
             }
+            //tady začíná část mocnin která bude upravena
+            else if (op == "224")
+            {
+                result = (c1 * c1) * c3;
+            }
+            //tady ta část končí
             else
             {
                 Console.WriteLine("Chyba, neplatná operace");
+                success = false;
             }
 
-            Console.WriteLine($"Výsledek: {result}");
-
-            Console.WriteLine("Děkuji za použití kalkulačky!");
-        }
-    }
-}
-
-/*
- * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
- * 
- * ZADANI
- * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
- * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
- * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
- * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
- * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
- * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
- * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
- *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
- * 7) Vypis promennou result do konzole
- * 
- * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
- * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
- * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
- * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
- *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
- * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
- */
-
-//Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
-
-Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+            if (success == false)
+            {
+                Console.WriteLine("Výpočet není možný, špatné zadání, zkuste to znovu");
+            }
+            else
+            {
+                Console.WriteLine($"Výsledek: {result}");
+                Console.WriteLine("Gratuluji, neplýtval jsi svou mysl na jednoduchý úkon počítání");
+            }
+            Console.ReadKey();
         }
     }
 }
