e77fda0564ac467a9fc4e414ae59a30f753d0ab8
Matyaselement <matyas.voboril@gmail.com>
2023-12-06 11:24:07 +0100

    Classes

---
+++ ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public int Width;
+        public int Height;
+
+        public Rectangle(int width, int height)
+        {
+            width = Width;
+            height = Height;
+        }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine("Pro spočítání obsahu obdélníku stiskněte 1");
+            string ifarea = Console.ReadLine();
+            if (ifarea == "1")
+            {
+                int area = Width * Height;
+                Console.WriteLine("Obsah obdélníku je",area);
+            }
+        }
+    }
+}
---
+++ tridy/tridy/human.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tridy
+{
+    internal class human
+    {
+        public int age;
+        public int height;
+        public int weight;
+        public string name;
+        public string skincolour;
+        public human partner;
+
+        public human(int age, int weight, string name, int height)
+        {
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.height = height;
+        }
+
+        public void IntroduceHuman();
+        {
+            Console.WriteLine("Jmenuji se" {name} "je mi" {age} "let. Měřím"{height})
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
