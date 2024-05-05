5dac58bf865d0427e3c6e59a770493f71c40b76e
Vaclaveskere <144894657+Vaclaveskere@users.noreply.github.com>
2024-05-05 22:33:37 +0200

    start, přidání některých class

--- CyberPrague/CyberPrague/Program.cs
+++ CyberPrague/CyberPrague/Program.cs
@@ -17,6 +17,8 @@ namespace CyberPrague
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
+       
     }
 }
