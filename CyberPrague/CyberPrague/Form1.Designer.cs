b9f6e1c240c34dfad089a042bd1814fc6a18b36c
matejmikula <144894481+matejmikula@users.noreply.github.com>
2024-05-29 09:48:09 +0200

    uprava boundu
    predelaní boundů tak aby se menili spolecne s velikosti okna, az budu mit background grafiku tak ji zkusim upravit stejne

--- CyberPrague/CyberPrague/Form1.Designer.cs
+++ CyberPrague/CyberPrague/Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.timerCoin = new System.Windows.Forms.Timer(this.components);
             this.updateCoinPick = new System.Windows.Forms.Timer(this.components);
             this.LabelCoinsGathered = new System.Windows.Forms.Label();
-            this.InventoryButton = new System.Windows.Forms.Button();
             this.weapon1 = new CyberPrague.weapon();
             this.player2 = new CyberPrague.player();
             this.player1 = new CyberPrague.player();
@@ -41,7 +40,7 @@
             // timerCoin
             // 
             this.timerCoin.Enabled = true;
-            this.timerCoin.Interval = 3000;
+            this.timerCoin.Interval = 10000;
             this.timerCoin.Tick += new System.EventHandler(this.timerCoin_Tick);
             // 
             // updateCoinPick
@@ -58,16 +57,6 @@
             this.LabelCoinsGathered.Size = new System.Drawing.Size(59, 20);
             this.LabelCoinsGathered.TabIndex = 1;
             this.LabelCoinsGathered.Text = "0 coins";
-            // 
-            // InventoryButton
-            // 
-            this.InventoryButton.Location = new System.Drawing.Point(141, 35);
-            this.InventoryButton.Name = "InventoryButton";
-            this.InventoryButton.Size = new System.Drawing.Size(262, 66);
-            this.InventoryButton.TabIndex = 2;
-            this.InventoryButton.Text = "Inventory";
-            this.InventoryButton.UseVisualStyleBackColor = true;
-            this.InventoryButton.Click += new System.EventHandler(this.InventoryButton_Click);
             // 
             // weapon1
             // 
@@ -102,10 +91,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1425, 757);
+            this.ClientSize = new System.Drawing.Size(978, 744);
             this.Controls.Add(this.weapon1);
             this.Controls.Add(this.player2);
-            this.Controls.Add(this.InventoryButton);
             this.Controls.Add(this.LabelCoinsGathered);
             this.Controls.Add(this.player1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -124,7 +112,6 @@
         private player player1;
         private System.Windows.Forms.Timer updateCoinPick;
         private System.Windows.Forms.Label LabelCoinsGathered;
-        private System.Windows.Forms.Button InventoryButton;
         private player player2;
         private weapon weapon1;
     }
